b1add34cc562bd0805197933447b1b3aac1d7d47
RevaGee <revoryoutub@gmail.com>
2024-07-25 17:55:14 +0300

    fixed bug with flat list on dashboard screen, added new item on this screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { styles } from './style.ts';
 import { login } from '../../services/api/auth.ts';
 import { posts } from '../../services/api/post.ts';
-import {Post} from "../../types/types.ts";
-
+import { Post } from '../../types/types.ts';
+import ListHeaderComponent from './listHeaderComponent.tsx';
 
 const Dashboard = () => {
   const [name, setName] = useState<string>('');
@@ -27,8 +27,14 @@ const Dashboard = () => {
   const getPosts = async () => {
     try {
       const postResponse = await posts();
-      setPostData(postResponse);
-    } catch (error) {}
+      const postsArray = Array.isArray(postResponse)
+        ? postResponse
+        : [postResponse];
+
+      setPostData(postsArray.slice(0, 3));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const renderItem = ({ item }: { item: Post }) => (
@@ -44,27 +50,13 @@ const Dashboard = () => {
         <Text style={style.childText}>Your name</Text>
         <Text style={style.title}>{`${name} ${lastName}`}</Text>
       </View>
-      <View style={style.container}>
-        <View style={style.textInputContainer}>
-          <View style={style.childContainer}>
-            <View style={style.icon} />
-            <Text style={style.textContainer}>John doe</Text>
-          </View>
-
-          <View style={style.inputContainer}>
-            <TouchableOpacity onPress={getPosts}>
-              <View style={style.childContainer}>
-                <Text style={style.textContainer}>Name</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-
-          <View style={{ margin: 5 }}>
-            <Text style={style.textPost}>Posts</Text>
-            <FlatList data={postData} renderItem={renderItem} />
-          </View>
-        </View>
-      </View>
+        <FlatList
+          ListHeaderComponent={ListHeaderComponent}
+          data={postData}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+        />
     </>
   );
 };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,10 @@
-
 export interface Post {
   id: number;
   title: string;
   body: string;
 }
+
+export interface PostResponse {
+  data: Post[];
+}
+export type PostResponse = Post[];
